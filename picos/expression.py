fac8374b82dd1ed1bc9cdc8b95ebcbc20f127da3
Guillaume Sagnol <sagnol@zib.de>
2014-07-09 14:33:25 +0200

    template removed from master branch

--- picos/expression.py
+++ picos/expression.py
@@ -2819,21 +2819,3 @@ class Variable(AffinExp):
                         raise IndexError('slice of zero-dimension')
                 return AffinExp(newfacs,newcons,newsize,newstr)
 
-class Affine_Uncertainty(Expression):
-        """
-        Affine expression with respect to :class:`Uparams<picos.Uparam>`.
-        This class derives from :class:`Expression<picos.Expression>`.
-        
-        
-        TODO: copy from Affine expression, but keys of the factor dictionary should be instances of Uparam instead of Variable
-        """
-        
-class UParam(Affine_Uncertainty):
-        """
-	Test: I hope you see this
-        An uncertainty parameter
-        This class derives from :class:`Affine_Uncertainty<picos.Affine_Uncertainty>`.
-       
-        
-        TODO: copy from Variable, the idea is that a Uparam 'u' should be stored as I*u + 0
-        """
