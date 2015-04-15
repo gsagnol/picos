0020729761a0eccdc7bd4823c6ebc349716883c6
Guillaume <sagnol@zib.de>
2015-04-15 13:48:27 +0200

    next dev version ready

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='PICOS',
-    version='1.1.0',
+    version='1.1.1dev',
     author='G. Sagnol',
     author_email='sagnol@zib.de',
     packages=['picos'],
